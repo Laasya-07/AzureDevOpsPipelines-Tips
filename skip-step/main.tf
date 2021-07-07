f85cdc388fd3e3129f271e65eb95b67e7bc6ef39
Laasya-07 <61192665+Laasya-07@users.noreply.github.com>
2021-07-07 16:08:23 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -13,3 +13,24 @@ resource "azurerm_resource_group" "rg" {
   name     = "resourcegroup-test-tbd-3"
   location = "westeurope"
 }
+
+resource "azurerm_storage_account" "strg" {
+  name                     = "storageacct"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "cont" {
+  name                  = "storagecontainer"
+  storage_account_name  = azurerm_storage_account.strg.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "blobobject" {
+   name = "index.html"
+   resource_group_name = azurerm_resource_group.rg.name
+   storage_account_name = azurerm_storage_account.strg.name
+   storage_container_name = azurerm_storage_container.cont.name
+}
