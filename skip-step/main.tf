34128a12153e842b146ffd66c79fd715bfc92513
Laasya-07 <61192665+Laasya-07@users.noreply.github.com>
2021-07-08 17:03:06 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -10,12 +10,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
+  name     = "resourcegroup-tfstate"
   location = "westeurope"
 }
 
 resource "azurerm_storage_account" "strg" {
-  name                     = "storageacct"
+  name                     = "terraformtfstateskip1"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -23,13 +23,13 @@ resource "azurerm_storage_account" "strg" {
 }
 
 resource "azurerm_storage_container" "cont" {
-  name                  = "storagecontainer"
+  name                  = "tfstate1"
   storage_account_name  = azurerm_storage_account.strg.name
   container_access_type = "blob"
 }
 
 resource "azurerm_storage_blob" "blobobject" {
-   name = "index.html"
+   name = "tfstate"
    resource_group_name = azurerm_resource_group.rg.name
    storage_account_name = azurerm_storage_account.strg.name
    storage_container_name = azurerm_storage_container.cont.name
